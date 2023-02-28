20373800ebaa338be9a994993e1e2e412ad2926f
Alexander Luna <alexfox_2000@hotmail.com>
2023-02-28 18:25:33 -0500

    add tweet-form to home route

--- src/components/page/Feed.jsx
+++ src/components/page/Feed.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Tweet from '../tweet/Tweet'
+import TweetForm from '../tweet/TweetForm'
 import { tweets } from './tweets-data'
 
 export default function Feed() {
+  const userHasLoggedIn = true;
+
   return (
     <div className='feed'>
         <h1 className='text-l text-bold home__title'>Home</h1>
+        {userHasLoggedIn && <TweetForm/>}
         <div className='feed-tweets'>
             {tweets.map(tweetContent => {
                 return <Tweet key={tweetContent.at_name} tweetContent={tweetContent}></Tweet>
--- src/components/tweet/TweetForm.jsx
+++ src/components/tweet/TweetForm.jsx
@@ -5,7 +5,7 @@ export default function TweetFrom() {
     <div className="tweet-form">
       <img className="tweet-form__avatar" src={thanosUrl} />
       <div className="tweet-form__content">
-        <input className="text-input" placeholder="Placeholder"></input>
+        <textarea className="tweet-form-textarea" placeholder="Remember to think before you write"></textarea>
         <button className="button">Tweet</button>
       </div>
     </div>
