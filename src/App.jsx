721cda05e884044049929d56beae229e6c9898bc
Alexander Luna <alexfox_2000@hotmail.com>
2023-02-28 18:35:26 -0500

    handle missing page error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './styles/App.css'
-import TweetForm from "./components/tweet/TweetForm";
-import Tweet from "./components/tweet/Tweet";
 import {Routes, Route} from 'react-router-dom'; 
 import Components from './routes/components/Components';
 import Home from './routes/home/Home';
+import MissingPage from './routes/status/MissingPage';
 
 function App() {
   return (
     <Routes>
+      <Route path="*" element={<MissingPage/>}></Route>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/components" element={<Components/>}></Route>
     </Routes>
